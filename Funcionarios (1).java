311466cec7923f478d2998987adbc3f3b9d0c151
joaogabrirl96 <joaogabriel.silva1573@gmail.com>
2022-03-20 21:48:12 -0300

    initial commit

---
+++ Funcionarios (1).java
@@ -0,0 +1,41 @@
+package Funcionarios;
+public class Funcionarios{
+private String nome;
+private int telefone;
+private boolean farda;
+private String email;
+private String contrato;
+public String getNome() {
+	return nome;
+	
+}
+public void setNome(String nome) {
+	this.nome = nome;
+}
+public int getTelefone() {
+	return telefone;
+}
+public void setTelefone(int telefone) {
+	this.telefone = telefone;
+} 
+public boolean isFarda() {
+	return farda;
+}
+public void setFarda(boolean farda) {
+this.farda = farda;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public String getContrato() {
+	return contrato;
+}
+public void setContrato(String contrato) {
+	this.contrato = 
+		contrato;
+}
+
+}
